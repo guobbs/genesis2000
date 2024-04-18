06b0b02d4b8f959c227bcf4021f9f3dc558bb10f
GUO <guobbs@live.com>
2024-04-18 21:34:16 +0800

    更新demo代码

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,14 +1,17 @@
-use std::{collections::HashMap, io::*};
+use std::collections::HashMap;
 
 use genesis2000::{Genesis, InfoParamType};
 
 fn main() {
     let mut f = Genesis::new();
     f.COM("units,type=mm");
-    std::io::stdout().write(f.STATUS.as_bytes()).unwrap();
-    std::io::stdout().write(f.COMANS.as_bytes()).unwrap();
+    println!("status: {}\ncomans: {}", f.STATUS, f.COMANS);
     f.COM("get_affect_layer");
-    std::io::stdout().write(f.COMANS.as_bytes()).unwrap();
+    println!("status: {}\ncomans: {}", f.STATUS, f.COMANS);
+    f.MOUSE("p Select a point on the screen");
+    println!("status: {}\nmouse: {}", f.STATUS, f.MOUSEANS);
+    f.MOUSE("r Select an rectangle area on the screen");
+    println!("status: {}\nmouse: {}", f.STATUS, f.MOUSEANS);
 
     let params: HashMap<InfoParamType, String> = HashMap::<InfoParamType, String>::from([
         (InfoParamType::EntityType, String::from("matrix")),
